aa2d0ba7bc322946d5000d7222ecb39318bb1577
sud7749 <sameersharma078626@gmail.com>
2020-07-16 23:14:14 +0800

    Initial Commit

---
+++ Newswav/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newswav
+//
+//  Created by Sudhanshu Sharma (HLB) on 16/07/2020.
+//  Copyright © 2020 Sudhanshu Sharma (HLB). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
